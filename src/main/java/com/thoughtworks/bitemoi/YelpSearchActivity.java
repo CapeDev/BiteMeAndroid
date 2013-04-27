beabae0d30f447e21021ca6a35f554f464679f8b
Toby Clemson <tobyclemson@gmail.com>
2013-04-27 13:12:15 -0700

    Andrew|Toby - Tidying up search naming.

--- src/main/java/com/thoughtworks/bitemoi/YelpSearchActivity.java
+++ src/main/java/com/thoughtworks/bitemoi/YelpSearchActivity.java
@@ -18,59 +18,56 @@ import java.util.List;
 
 public class YelpSearchActivity extends Activity {
 
-	private ListView mSearchResultsText;
+    private ListView searchResultsView;
 
-	@Override
-	public void onCreate(Bundle b) {
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		super.onCreate(b);
-		setContentView(R.layout.search);
-		setTitle("Food search");
-	}
+    @Override
+    public void onCreate(Bundle b) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        super.onCreate(b);
+        setContentView(R.layout.search);
+        setTitle("Food search");
+    }
 
-	protected List<String> processJson(String jsonStuff) throws JSONException {
-		JSONObject json = new JSONObject(jsonStuff);
-		JSONArray businesses = json.getJSONArray("businesses");
-		ArrayList<String> businessNames = new ArrayList<String>(
-				businesses.length());
-		for (int i = 0; i < businesses.length(); i++) {
-			JSONObject business = businesses.getJSONObject(i);
-			businessNames.add(business.getString("name"));
-		}
-		return businessNames;
-	}
+    protected List<String> processJson(String jsonStuff) throws JSONException {
+        JSONObject json = new JSONObject(jsonStuff);
+        JSONArray businesses = json.getJSONArray("businesses");
+        ArrayList<String> businessNames = new ArrayList<String>(
+                businesses.length());
+        for (int i = 0; i < businesses.length(); i++) {
+            JSONObject business = businesses.getJSONObject(i);
+            businessNames.add(business.getString("name"));
+        }
+        return businessNames;
+    }
 
-	public void searchForRestaurant(View v) {
-		EditText searchKey = (EditText) findViewById(R.id.search);
-		final String val = searchKey.getText().toString();
-		mSearchResultsText = (ListView) findViewById(R.id.searchResult);
+    public void searchForRestaurant(View v) {
+        EditText searchKey = (EditText) findViewById(R.id.search);
+        final String val = searchKey.getText().toString();
+        searchResultsView = (ListView) findViewById(R.id.searchResult);
 
-		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1);
-		mSearchResultsText.setAdapter(adapter);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        searchResultsView.setAdapter(adapter);
 
-        adapter.add("It works!!!!");
+        setProgressBarIndeterminateVisibility(true);
+        new AsyncTask<Void, Void, List<String>>() {
+            @Override
+            protected List<String> doInBackground(Void... params) {
+                YelpProxy yelp = YelpProxy.getYelp(YelpSearchActivity.this);
+                String businesses = yelp.search(val, 37.788022, -122.399797);
+                try {
+                    return processJson(businesses);
+                } catch (JSONException e) {
+                    return null;
+                }
+            }
 
-		setProgressBarIndeterminateVisibility(true);
-		new AsyncTask<Void, Void, List<String>>() {
-			@Override
-			protected List<String> doInBackground(Void... params) {
-				YelpProxy yelp = YelpProxy.getYelp(YelpSearchActivity.this);
-				String businesses = yelp.search(val, 37.788022, -122.399797);
-				try {
-					return processJson(businesses);
-				} catch (JSONException e) {
-					return null;
-				}
-			}
+            @Override
+            protected void onPostExecute(List<String> result) {
+                adapter.addAll(result);
+                setProgressBarIndeterminateVisibility(false);
+            }
+        }.execute();
 
-			@Override
-			protected void onPostExecute(List<String> result) {
-				adapter.addAll(result);
-				setProgressBarIndeterminateVisibility(false);
-			}
-		}.execute();
-
-	}
+    }
 
 }
