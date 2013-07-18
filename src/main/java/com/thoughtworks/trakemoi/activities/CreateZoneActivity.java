bff260008f86821c126f1d124cb7831096f7c7cc
Vanessa Towers <vanessa.towers@gmail.com>
2013-07-18 00:30:51 -0700

    Modified keyboard input for latitude/longitude/radius text fields to only accept double|int/double|int/int respectively. Made latitude/longitude/radius fields not editable for the time being, this might change.

--- src/main/java/com/thoughtworks/trakemoi/activities/CreateZoneActivity.java
+++ src/main/java/com/thoughtworks/trakemoi/activities/CreateZoneActivity.java
@@ -1,14 +1,13 @@
 package com.thoughtworks.trakemoi.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.EditText;
 import android.widget.Toast;
 import com.thoughtworks.trakemoi.R;
 
@@ -21,6 +20,15 @@ public class CreateZoneActivity extends TrakemoiActivity {
         setTitle("Create Zone");
         setContentView(R.layout.create_zone);
         setUpActionBar();
+
+        EditText latitudeInput = (EditText) findViewById(R.id.latitude_input);
+        latitudeInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        EditText longitudeInput = (EditText) findViewById(R.id.longitude_input);
+        longitudeInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        EditText radiusInput = (EditText) findViewById(R.id.radius_input);
+        radiusInput.setInputType(InputType.TYPE_CLASS_NUMBER);
     }
 
     @Override
