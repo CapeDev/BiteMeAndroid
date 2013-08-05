64b0a2106136336768baab8f4a9d7a96501dc16e
Vanessa Towers <vanessa.towers@gmail.com>
2013-08-04 19:33:07 -0700

    Worked on flow to create a zone, save to sqlite. New dialogs on save button in LocationActivity. Needs refactoring.

---
+++ src/main/java/com/thoughtworks/trakemoi/dialog/SaveZoneDialogFragment.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.trakemoi.dialog;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThoughtWorks
+ * Date: 8/4/13
+ * Time: 6:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SaveZoneDialogFragment {
+}
---
+++ src/main/java/com/thoughtworks/trakemoi/models/Zone.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.trakemoi.models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ThoughtWorks
+ * Date: 8/4/13
+ * Time: 1:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Zone {
+}
